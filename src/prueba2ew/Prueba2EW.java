bf45b8c75a57f4a49f15bc9582b8081889e69f90
atejidosolla <atejidosolla@atanasoff29.danielcastelao.org>
2016-10-24 08:56:28 +0200

    Sin modificar

---
+++ src/prueba2ew/Prueba2EW.java
@@ -0,0 +1,16 @@
+
+package prueba2ew;
+
+/**
+ *
+ * @author atejidosolla
+ */
+public class Prueba2EW {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Esta es una prueba de ew 
+    }
+    
+}
